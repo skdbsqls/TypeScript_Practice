2dcbe1492f6aa506e772580b1e9299257c63529e
skdbsqls <skdbsqls98@naver.com>
2023-07-27 17:46:04 +0900

    배열과 객체 다루기 예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Practice from "./components/Practice2";
+import Practice2 from "./components/Practice";
 
 function App() {
   return (
     <>
       <Counter />
+      <Practice />
+      <Practice2 />
     </>
   );
 }
